832cc333f252f6f6d386922678fef5805b4d371f
Arseniy Seroka <ars.seroka@gmail.com>
2014-12-22 00:00:04 +0300

    modify: default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 with import <nixpkgs> {};
 
+with stdenv;
+with pkgs;
+
 buildPythonPackage {
     name = "VkParser";
-    buildInputs = with pkgs.pythonPackages; [ json, requests, collections, binascii, time ];
-    src = fetchurl {
-        url = "https://github.com/Igorjan94/vk/blob/master/vkparser.py";
-        sha256 = "ffc967e4811bb384bbbdf7bfd736228c281d320a14cea7f578039da51063db4d";
-    };
+    buildInputs = with pkgs.pythonPackages; [ requests time ];
+    src = ./.;
 }
